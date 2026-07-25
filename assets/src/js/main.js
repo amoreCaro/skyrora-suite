25544cad219259f4b5f7f6767e7227737e75d8ce
amoreCaro <admin@MacBook-Pro-3.local>
2026-07-25 14:19:57 +0200

    style(admin): add styles for template page

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,10 +1,12 @@
 import '../css/main.css';
 import { subscriberAdmin } from './components/subscriber-admin/index.js';
+import { emailAdmin } from './components/email-admin/index.js';
 import { listAdmin } from './components/list-admin/index.js';
 import { sendPage, mailingNextButton } from './components/mailing-send/index.js';
 
 const init = () => {
     subscriberAdmin();
+    emailAdmin();
     listAdmin();
     sendPage();
     mailingNextButton();
